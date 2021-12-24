a9a2b3648eb29c1ac5a0a4dadb33ffd9d2ccb004
ApotoxinElish <19079181d@connect.polyu.hk>
2021-12-24 22:33:56 +0800

    Update

---
+++ Assets/Scripts/UIFramework/StartPanel.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 开始主面板
+/// </summary>
+public class StartPanel : BasePanel
+{
+    static readonly string path = "Prefabs/UI/Panel/StartPanel";
+
+    public StartPanel() : base(new UIType("")) { }
+}
